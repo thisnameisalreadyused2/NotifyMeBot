16887f00b00268281c1e13bc590eaa8ee136a857
Yeami <nik2x0@gmail.com>
2019-05-25 19:24:04 +0300

    Implemented basic user registration

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import logging
 import os
 
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, ConversationHandler, CallbackQueryHandler, MessageHandler, Filters
 
 import modules
 
@@ -9,6 +9,18 @@ import modules
 updater = Updater(token=os.environ['TELEGRAM_BOT_TOKEN'], use_context=True)
 dp = updater.dispatcher
 
-dp.add_handler(CommandHandler(['start'], modules.start_command, pass_args=True))
+conv_handler = ConversationHandler(
+        entry_points=[CommandHandler(['start'], modules.start_command, pass_args=True)],
+
+        states={
+            modules.LANG: [CallbackQueryHandler(modules.lang_command)],
+            modules.TIME_ZONE: [MessageHandler(Filters.text, modules.time_zone_handler)],
+            modules.MENU: [MessageHandler(Filters.text, modules.menu_handler)]
+        },
+
+        fallbacks=[CommandHandler(['cancel'], modules.cancel_command)]
+    )
+
+dp.add_handler(conv_handler)
 
 updater.start_polling()
--- modules/__init__.py
+++ modules/__init__.py
@@ -1,1 +1,3 @@
-from ._start import start_command
+from ._start import start_command, lang_command, time_zone_handler, menu_handler, cancel_command
+from ._start import LANG, TIME_ZONE, MENU
+
