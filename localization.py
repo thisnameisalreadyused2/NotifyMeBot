1b0f8ad48826df6263e3c7f13780af2a95ce8e45
BoberMod <BoberMod@yandex.ru>
2019-05-29 22:22:55 +0300

    Update localication, added more languages

--- localization.py
+++ localization.py
@@ -1,74 +1,92 @@
 languages = {
     "en": {
-        "hello": "Hello, I'm notifier bot!\nPlease choose the language that you know better :3",
-        "write_time_zone": "You selected *English* language.\nNow set your time zone, please.",
+        "hello": "Hello, I'm notifier bot!"
+                 "\nPlease, select the interface language that is the most convenient for you.",
+        "write_time_zone": "You selected *English* language.\nNow set your timezone, please.",
         "description": "The short description of the menu:\n"
-                       "\n*Events* — will show you the list of all your events."
-                       "\n*Settings* — will show you your settings and give the opportunity to change them.",
-        "time_zone": "Your time zone is UTC ",
+                       "\n*Add Event* — will add new event to the database."
+                       "\n*Show Events* — will show you the list of all your events."
+                       "\n*Settings* — will show your current settings and give the opportunity to change them.",
+        "time_zone": "Your timezone is UTC ",
         "menu_add_events": "✨ Add Event",
         "menu_show_events": "🎈 Show Events",
         "menu_settings": "⚙️ Settings",
+        "settings_info": "Choose the item to configure:",
         "birthday_info": "Birthday Notifications",
         "birthday_settings": "⚙️",
         "common_info": "Common Notifications",
         "common_settings": "⚙️",
         "facebook_info": "Facebook Notifications",
         "facebook_settings": "⚙️",
-        "language_change": "Change the language.",
-        "time_zone_change": "Change the time zone.",
-        "settings_info": "Choose the item to configure:",
-        "change_language": "Please, choose new language:",
-        "choose_event_action": "Please, choose the action:",
+        "language_change": "Change the language",
+        "time_zone_change": "Change the timezone",
+        #"change_language": "Please, choose new language:",
+        #"choose_event_action": "Please, choose the action:",
         "add_new_events": "Add new Event",
         "show_all_events": "Show all Events",
         "enter_event_text": "Please, enter the event information:",
         "added_successfully": "Notification added successfully!",
-        "back": "🔙 Back",
+        #"back": "🔙 Back",
         "bye": "Bye! I hope we can talk again some day."
     },
     "ua": {
-        "hello": "Привіт, я бот повідомлючав!\nБудь ласка оберіть мову спілкування, яку знаєте найкраще :3",
-        "write_time_zone": "You selected *English* language.\nNow set your time zone, please.",
-        "description": "The short description of the menu:\n"
-                       "\n*Events* — will show you the list of all your events."
-                       "\n*Settings* — will show you your settings and give the opportunity to change them.",
-        "time_zone": "Your time zone is UTC ",
-        "menu_events": "🎈 Events",
+        "hello": "Привіт, я бот, який буде нагадувати Вам про все!"
+                 "\nБудь ласка оберіть найзручнішу мову спілкування для Вас.",
+        "write_time_zone": "Ви обрали *українську* мову.\nТепер встановіть Ваш часовий пояс, будь ласка.",
+        "description": "Короткий опис пунктів менюпраоарпоарпо еероглдпраопр:\n"
+                       "\n*Add Event* — додасть нове нагадування."
+                       "\n*Show Events* — відобразить всі існуючі нагадування."
+                       "\n*Settings* — відобразить поточні налаштування і дає змогу їх налаштувати.",
+        "time_zone": "Ваш часовий пояс UTC ",
+        "menu_add_events": "✨ Add Event",
+        "menu_show_events": "🎈 Show Events",
         "menu_settings": "⚙️ Settings",
-        "birthday_info": "Birthday Notifications",
-        "birthday_settings": "⚙️",
-        "common_info": "Common Notifications",
-        "common_settings": "⚙️",
-        "facebook_info": "Facebook Notifications",
-        "facebook_settings": "⚙️",
-        "language_change": "Change the language",
-        "time_zone_change": "Change the time zone",
         "settings_info": "Оберіть пункт для налаштування:",
-        "change_language": "Please, choose the new language:",
-        "back": "🔙 Back",
-        "bye": "Bye! I hope we can talk again some day."
+        "birthday_info": "Дні народження",
+        "birthday_settings": "⚙️",
+        "common_info": "Звичайні нагадування",
+        "common_settings": "⚙️",
+        "facebook_info": "Facebook нагадування",
+        "facebook_settings": "⚙️",
+        "language_change": "Змінити мову інтерфейсу",
+        "time_zone_change": "Змінити часовий пояс",
+        #"change_language": "Please, choose new language:",
+        #"choose_event_action": "Please, choose the action:",
+        "add_new_events": "Add new Event",
+        "show_all_events": "Show all Events",
+        "enter_event_text": "Будь ласка, введіть інформацію про нове нагадування:",
+        "added_successfully": "Нагадування було додато уснішно!",
+        #"back": "🔙 Back",
+        "bye": "Прощавай! Сподіваюсь коли-небудь ще зустрінемось."
     },
     "ru": {
-        "hello": "Привет, я бот уведомитель!\nПожалуйста выберете язык, который знаете лучше всего :3",
-        "write_time_zone": "You selected *English* language.\nNow set your time zone, please.",
-        "description": "The short description of the menu:\n"
-                       "\n*Events* — will show you the list of all your events."
-                       "\n*Settings* — will show you your settings and give the opportunity to change them.",
-        "time_zone": "Your time zone is UTC ",
-        "menu_events": "🎈 Events",
+        "hello": "Привет, я бот, который будет напоминать Вам обо всём!"
+                 "\nПожалуйста, выберите самый удобный язык интерфейса для Вас.",
+        "write_time_zone": "Вы выбрали *русский* язык.\nТеперь установите Ваш часовой пояс, пожалуйста.",
+        "description": "Короткое описание пунктов меню:\n"
+                       "\n*Add Event* — добавит новое напоминание."
+                       "\n*Show Events* — отобразит все существующие напоминания."
+                       "\n*Settings* — отобразит текущие настройки и даст возможность их изменить.",
+        "time_zone": "Ваш часовой пояс UTC ",
+        "menu_add_events": "✨ Add Event",
+        "menu_show_events": "🎈 Show Events",
         "menu_settings": "⚙️ Settings",
-        "birthday_info": "Birthday Notifications",
+        "settings_info": "Выберите пункт для настройки:",
+"birthday_info": "Дни рождения",
         "birthday_settings": "⚙️",
-        "common_info": "Common Notifications",
+        "common_info": "Обычные напоминания",
         "common_settings": "⚙️",
-        "facebook_info": "Facebook Notifications",
+        "facebook_info": "Facebook напоминания",
         "facebook_settings": "⚙️",
-        "language_change": "Change the language",
-        "time_zone_change": "Change the time zone",
-        "settings_info": "Choose the item to configure:",
-        "change_language": "Please, choose the new language:",
-        "back": "🔙 Back",
-        "bye": "Bye! I hope we can talk again some day."
+        "language_change": "Изменить язык интерфейса",
+        "time_zone_change": "Изменить часовой пояс",
+        #"change_language": "Please, choose new language:",
+        #"choose_event_action": "Please, choose the action:",
+        "add_new_events": "Add new Event",
+        "show_all_events": "Show all Events",
+        "enter_event_text": "Пожалуйста, введите информацию про новое напоминание:",
+        "added_successfully": "Напоминание добавлено успешно!",
+        #"back": "🔙 Back",
+        "bye": "Прощай! Надеюсь когда-нибудь ещё увидимся."
     }
 }
